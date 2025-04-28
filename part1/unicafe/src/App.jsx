8c0829e51d2d02c1b8b3edb3f4883444ddb1a06b
Elizabeth Nolte <elnccb@umsystem.edu>
2025-04-28 07:08:58 -0500

    Exercises 1.6-1.11 complete

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,78 @@
+import { useState } from 'react'
+
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
+const StatisticLine = ({text, value}) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+const Statistics = ({good, neutral, bad, total}) => {
+  if(total !== 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="all" value={total} />
+            <StatisticLine text="average" value={(1*good - 1*bad) / total} />
+            <StatisticLine text="positive" value={good * 100 /total + " %"} />
+          </tbody>
+        </table>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  const handleGoodClick = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    setTotal(updatedGood + neutral + bad)
+  }
+
+  const handleNeutralClick = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    setTotal(updatedNeutral + good + bad)
+  }
+
+  const handleBadClick = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    setTotal(updatedBad + neutral + good)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={handleGoodClick} text="good" />
+      <Button onClick={handleNeutralClick} text="neutral" />
+      <Button onClick={handleBadClick} text="bad" />
+
+      <Statistics good={good} neutral={neutral} bad={bad} total={total}/>
+    </div>
+  )
+}
+
+export default App
