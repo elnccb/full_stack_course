323adcce5417ab3189a04b34e6bc994c15e41b01
Elizabeth Nolte <elnccb@umsystem.edu>
2025-05-01 13:13:52 -0500

    uibuild

--- part2/phonebook/vite.config.js
+++ part2/phonebook/vite.config.js
@@ -4,12 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        configure: (proxy) => {
+          proxy.on('proxyReq', (proxyReq, req) => {
+            console.log(`[proxy] ${req.method} ${req.url} -> ${proxyReq.getHeader('host')}`)
+          })
+        }
       },
     }
   },
